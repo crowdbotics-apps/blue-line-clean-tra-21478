24f2693b92a06808ae5d2d32b56284dbd9cdd337
Crowdbotics <team@crowdbotics.com>
2020-10-14 03:37:42 +0000

    Update models

--- backend/blue_line_clean_tra_21478/wsgi.py
+++ backend/blue_line_clean_tra_21478/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blue_line_clean_tra_21478.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blue_line_clean_tra_21478.settings")
 
 application = get_wsgi_application()
